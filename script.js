5359eaeb5df9070054543b92d215c1e09642f15d
matthew <127mwilliams@frhsd.com>
2025-11-17 12:12:53 -0500

    bgbgb

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const selector = document.getElementById("postcard");
+/*const selector = document.getElementById("postcard");
 
 document.getElementById("states").addEventListener("change", function() {
   var selectedState = this.value;
@@ -10,4 +10,21 @@ document.getElementById("states").addEventListener("change", function() {
   document.getElementById("greeting").innerHTML = "Greetings from";
   document.getElementById("state-heading").innerHTML = "" + selectedState;
 }
-);
+); */
+
+const selector = document.getElementById('postcard');
+const dropdown = document.getElementById("states");
+const Generate = document.getElementById("greeting")
+const choice = document.getElementById("state-heading")
+document.getElementById('states').addEventListener("change", function() {
+    var selectedState = this.value;
+    var imagePath = "img/" + selectedState + "@2x.jpg";
+    selector.style.backgroundImage = "url('" + imagePath + "')";
+    selector.alt = selectedState;
+    document.getElementById("greeting").text = ("Greetings from");
+    
+    document.getElementById("greeting").innerHTML = "Greetings from";
+    document.getElementById("state-heading").innerHTML = [selectedState];
+} 
+)
+
